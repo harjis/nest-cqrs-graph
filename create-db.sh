d1df758a045aa76a2b2559ce7dbefb07550f85e4
Joonas Harjukallio <harjukallio@gmail.com>
2020-04-27 12:20:10 +0300

    Docker

---
+++ create-db.sh
@@ -0,0 +1,1 @@
+docker exec -it nest-cqrs-graph_postgres_1 createdb -U postgres 'nest-cqrs-graph_dev'
--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'postgres',
   port: 5432,
   username: 'postgres',
   password: 'postgres',
-  database: 'nest-graph_dev',
+  database: 'nest-cqrs-graph_dev',
   entities: ['dist/**/*.entity.js'],
   synchronize: true,
   logging: true
